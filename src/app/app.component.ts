7e4fe57a2d86905f0d660c403a129410355d65d3
rushi <rushi.2106@gmail.com>
2020-05-25 21:09:34 +0530

    API integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,95 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'file-upload-angular-demo';
+  fileUploadUrl = 'https://localhost:44381/fileupload'
+  employee: EmployeeAC = new EmployeeAC();
+  resumes;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  fileChange(event) {
+    this.resumes = event.target.files;
+  }
+
+  uploadFile1() {
+    let url = `${this.fileUploadUrl}/1`;
+
+    var formData = new FormData();
+    formData.append("file", this.resumes[0]);
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('done');
+    })
+  }
+
+  uploadFile2() {
+    let url = `${this.fileUploadUrl}/2`;
+
+    var formData = new FormData();
+    formData.append("file", this.resumes[0]);
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('done');
+    })
+  }
+
+  uploadFile3() {
+    let url = `${this.fileUploadUrl}/3`;
+
+    var formData = new FormData();
+
+    for (var i = 0; i < this.resumes.length; i++) {
+      formData.append("files", this.resumes[i]);
+    }
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('done');
+    })
+  }
+
+  uploadFile4() {
+    let url = `${this.fileUploadUrl}/4`;
+
+    var formData = this.objectToFormData(this.employee);
+
+    formData.append('resume', this.resumes[0]);
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('done');
+    })
+  }
+
+  uploadFile5() {
+    let url = `${this.fileUploadUrl}/5`;
+
+    var formData = this.objectToFormData(this.employee);
+
+    for (var i = 0; i < this.resumes.length; i++) {
+      formData.append("resumes", this.resumes[i]);
+    }
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('done');
+    })
+  }
+
+  private objectToFormData(object) {
+    var formData = new FormData();
+
+    for (var key in object) {
+      formData.append(key, object[key]);
+    }
+
+    return formData;
+  }
+}
+
+export class EmployeeAC {
+  name: string;
+}
+
+export class EmployeeWithMultipleResumeAC {
+  name: string;
 }
